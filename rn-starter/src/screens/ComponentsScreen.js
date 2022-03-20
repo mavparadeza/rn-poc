ba94ad04d58c0ce70d1307a912f94f57efc10d95
Mav Paradeza <17306406+mavparadeza@users.noreply.github.com>
2022-03-20 17:17:12 +0800

    updates component screen, list

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle} >This is the components screen.</Text>
+  const greeting = "Hello, there!"
+  const name = "mav"
+  return (
+    <View>
+      <Text style={styles.textStyle} >This is the components screen.</Text>
+      <Text style={styles.subHeaderStyle}>I am {name}.</Text>
+    </View>
+  ) 
+  
+ 
 };
 
 const styles = StyleSheet.create({
   textStyle: {
     fontSize: 50
+  },
+  subHeaderStyle: {
+    fontSize: 20
   }
 });
 
